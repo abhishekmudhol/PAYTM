dd4658b8522101939217dad83f5e4b5d5d2e44b0
abhishekmudhol <104548603+abhishekmudhol@users.noreply.github.com>
2024-10-17 15:27:58 +0530

    added tailwind to app

---
+++ apps/merchant-app/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@repo/ui/button";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className="text-orange-500">
+       ABHISHEK
+    </div>
+  )
+}
