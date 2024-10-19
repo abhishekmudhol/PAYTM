d0ede42b51ba238285a13a74852a9a612ad97018
abhishekmudhol <104548603+abhishekmudhol@users.noreply.github.com>
2024-10-20 00:00:53 +0530

    fixed bug in p2p transfer
    lock row in balance table

--- apps/user-app/components/SendCard.tsx
+++ apps/user-app/components/SendCard.tsx
@@ -5,6 +5,7 @@ import { Button } from "@repo/ui/button";
 import { Card } from "@repo/ui/card";
 import { Center } from "@repo/ui/center";
 import { useState } from "react";
+import { p2pTransfer } from "@/lib/actions/p2pTransfer";
 
 export function SendCard() {
     const [number, setNumber] = useState("");
@@ -21,8 +22,8 @@ export function SendCard() {
                         setAmount(value)
                     }} />
                     <div className="pt-4 flex justify-center">
-                        <Button onClick={() => {
-
+                        <Button onClick={async() => {
+                            await p2pTransfer(number, Number(amount) * 100)
                         }}>Send</Button>
                     </div>
                 </div>
--- apps/user-app/lib/actions/p2pTransfer.ts
+++ apps/user-app/lib/actions/p2pTransfer.ts
@@ -26,9 +26,19 @@ export async function p2pTransfer(to: string, amount: number) {
         }
     }
 
+    if (Number(from) == Number(toUser.id)) {
+        return {
+            message: `Error: sending it to ourself, i.e. to == from`
+        }
+    }
+
     try {
         await client.$transaction(async (tx) => {
             // Fetch sender's balance within the transaction
+
+            //TODO: lock the row form Balance table where userId = from
+            await tx.$queryRaw`SELECT * FROM "Balance" WHERE "userId" = ${Number(from)} FOR UPDATE`;
+            
             const fromBalance = await tx.balance.findUnique({
                 where: {
                     userId: Number(from)
@@ -38,7 +48,7 @@ export async function p2pTransfer(to: string, amount: number) {
             if (!fromBalance || fromBalance.amount < amount) {
                 throw new Error('Insufficient funds');
             }
-
+            await new Promise(r => setTimeout(r, 4000));
             // Decrement the sender's balance
             await tx.balance.update({
                 where: {
